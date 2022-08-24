971266fab5c5d23da8090c5f9cff04493303d960
Étor Lucca <etor.lucca78@gmail.com>
2022-08-24 12:10:04 -0300

    Adição de funcionalidade: excluir projeto

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,33 @@
 import App from "./App.vue";
 import { createApp } from "vue";
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faPlay, faStop, faBusinessTime, faListCheck, faDiagramProject, faPlus, faPencilAlt } from '@fortawesome/free-solid-svg-icons'
-import roteador from './roteador';
-import { key, store } from './store';
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import {
+  faPlay,
+  faStop,
+  faBusinessTime,
+  faListCheck,
+  faDiagramProject,
+  faPlus,
+  faPencilAlt,
+  faTrash,
+} from "@fortawesome/free-solid-svg-icons";
+import roteador from "./roteador";
+import { key, store } from "./store";
 
-library.add(faPlay, faStop, faBusinessTime, faListCheck, faDiagramProject, faPlus, faPencilAlt)
+library.add(
+  faPlay,
+  faStop,
+  faBusinessTime,
+  faListCheck,
+  faDiagramProject,
+  faPlus,
+  faPencilAlt,
+  faTrash
+);
 
 createApp(App)
   .use(roteador)
   .use(store, key)
-  .component('font-awesome-icon', FontAwesomeIcon)
-  .mount('#app')
+  .component("font-awesome-icon", FontAwesomeIcon)
+  .mount("#app");
--- src/views/projetos/Lista.vue
+++ src/views/projetos/Lista.vue
@@ -24,6 +24,11 @@
                 <font-awesome-icon icon="fa-solid fa-pencil-alt" />
               </span>
             </router-link>
+            <button class="button ml-2 is-danger" @click="excluir(projeto.id)">
+              <span class="icon is-small">
+                <font-awesome-icon icon="fa-solid fa-trash" />
+              </span>
+            </button>
           </td>
         </tr>
       </tbody>
@@ -34,13 +39,20 @@
 <script lang="ts">
 import { computed, defineComponent } from "vue";
 import { useStore } from "@/store";
+import { EXCLUIR_PROJETO } from "@/store/tipo-mutacoes";
 
 export default defineComponent({
   name: "ListaVue",
+  methods: {
+    excluir(id: string) {
+      this.store.commit(EXCLUIR_PROJETO, id)
+    }
+  },
   setup() {
     const store = useStore()
     return {
-      projetos: computed(() => store.state.projetos)
+      projetos: computed(() => store.state.projetos),
+      store
     }
   }
 });
